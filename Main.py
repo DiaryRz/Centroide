8fc49626f1b6d589fb3e68229e636b53bfe9e3ac
diaryRz <diary200003@gmail.com>
2024-08-23 17:44:23 +0200

    Optimisation des routes

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.cluster import DBSCAN
+from Find import find_closest_point
 
 def calculate_centroid(cluster):
     latitudes = [point[0] for point in cluster]
@@ -8,24 +9,7 @@ def calculate_centroid(cluster):
     centroid_lon = sum(longitudes) / len(longitudes)
     return (centroid_lat, centroid_lon)
 
-def getCentroide(data, distance_max, min_point_gpe):
-    coordinates = transform_data(data)
-    coords_in_radians = np.radians(coordinates)
-    db = DBSCAN(eps=distance_max/6371.0, min_samples=min_point_gpe, algorithm='ball_tree', metric='haversine').fit(coords_in_radians)
-    labels = db.labels_
-    unique_labels = set(labels)
-    centroids = []
-    clusters = []
-    for label in unique_labels:
-        if label == -1:
-            continue
-        cluster_points = [coordinates[i] for i in range(len(coordinates)) if labels[i] == label]
-        clusters.append(cluster_points)
-        centroid = calculate_centroid(cluster_points)
-        centroids.append(centroid)
-    return centroids, clusters
-
-def getCentroidePerCluster(data, distance_max, min_point_gpe):
+def getCentroidePerCluster(data, distance_max=100, min_point_gpe=1):
     coordinates = transform_data(data)
     coords_in_radians = np.radians(coordinates)
     db = DBSCAN(eps=distance_max/6371.0, min_samples=min_point_gpe, algorithm='ball_tree', metric='haversine').fit(coords_in_radians)
@@ -52,3 +36,21 @@ def transform_data(data):
     coordinates = [(item["lat"], item["lon"]) for item in data_field]
     return coordinates
 
+def get_stop(listData):
+    processed_data = []
+    for item in listData:
+        result = find_closest_point(item.get("lat"), item.get("lon"))
+        jsondata = {
+            "lat": result.get("lat"),
+            "lon": result.get("lon"),
+            "people": item.get("people")
+        }
+        processed_data.append(jsondata)
+    print(processed_data)
+    return processed_data
+
+
+def union(data):
+    print(getCentroidePerCluster(data))
+    dt = getCentroidePerCluster(data)
+    get_stop(dt)
--- app.py
+++ app.py
@@ -1,13 +1,29 @@
 from flask import Flask, jsonify, request
-from Main import getCentroidePerCluster
+from Find import find_closest_point
+from Main import get_stop, getCentroidePerCluster, union
 
 app = Flask(__name__)
 
 @app.route('/api/listCentroide', methods=['POST'])
-def add_user():
+def listCentroide():
     data = request.get_json() 
-    response = getCentroidePerCluster(data,100,2)
+    distance_max = 100
+    min_people_per_group = 1
+    response = getCentroidePerCluster(data,distance_max,min_people_per_group)
     return jsonify(response), 201
+
+@app.route('/api/nearest_point', methods=['POST'])
+def nearest_point():
+    data = request.get_json() 
+    response = find_closest_point(data.get("lat") , data.get("long"))
+    print(response)
+    return jsonify(response) , 201
+
+@app.route('/api/plus_proche', methods=['POST'])
+def plus_proche():
+    data = request.get_json() 
+    union(data)
+    return "ok" , 201
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
